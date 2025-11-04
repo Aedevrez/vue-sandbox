0a4c7a40d43d337cda43dc7b01f544acd77dc884
Aedevrez <adevrez77@gmail.com>
2025-11-04 13:23:30 +0300

    Initial commit

---
+++ js/components/Assignments.js
@@ -0,0 +1,32 @@
+import AssignmentList from "./AssignmentList.js";
+
+export default {
+    components: { AssignmentList },
+    template: `
+        <section class="space-y-6">
+            <assignment-list :assignments="filters.pending" title="Pending Assignments"></assignment-list>
+            <assignment-list :assignments="filters.completed" title="Completed Assignments"></assignment-list>
+        </section>
+        
+    `,
+
+    data() {
+        return {
+            assignments: [
+                { name: "Finish Projects", complete: false, id: 1},
+                { name: "Read Chapter 4", complete: false, id: 2},
+                { name: "Perform Homework", complete: false, id: 3},
+            ]
+        }
+    },
+
+    computed: {
+        filters() {
+            return {
+                pending: this.assignments.filter(a => ! a.complete),
+                completed: this.assignments.filter(a => a.complete)
+            };
+        }
+    }
+    
+}
